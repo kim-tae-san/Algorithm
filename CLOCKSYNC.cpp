967f8dcaf84900fdf84360dd80c19ebd7bf24ffd
kimtaesan <ekdlqld21@naver.com>
2021-07-21 18:18:41 +0900

    7/21 종만북 CLOCKSYNC 완전탐색

--- CLOCKSYNC.cpp
+++ CLOCKSYNC.cpp
@@ -17,36 +17,45 @@ char connected[SWITCHES][CLOCKS+1] = {
     ".xxxxx..........",
     "...xxx...x...x.."
 };
-void move(vector <int> &direction, int place){
-    
-}
-int click(vector <int> &direction, int cnt){
+bool areAligned(vector<int>& direction){
     bool ok = true;
-    for(int i=0; i<CLOCKS; i++){
+    for(int i=0; i< CLOCKS; i++){
         if(direction[i] != 12){
             ok = false;
             break;
         }
     }
-    if(ok){
-        return cnt;
-    }
-    int ret = 0;
-    for(int i=0; i<4; i++){
-        for(int j=0; j<SWITCHES; j++){
-            
+    return ok;
+}
+void move(vector <int> &direction, int swtch){
+    for(int clock = 0; clock < CLOCKS; clock++){
+        if(connected[swtch][clock] == 'x'){
+            direction[clock] += 3;
+            if(direction[clock] == 15) direction[clock] = 3;
         }
     }
 }
+int click(vector <int> &direction, int swtch){
+    if(swtch == SWITCHES) return areAligned(direction)?0:INF;
+    int ret = INF;
+    for(int i=0; i<4; i++){
+        ret = min(ret, i + click(direction, swtch + 1));
+        move(direction, swtch);
+    }
+    return ret;
+}
 int main(){
     int C;
-    vector <int> direction;
     cin >> C;
     for(int i=0; i< C; i++){
+        vector <int> direction;
         for(int j=0; j<CLOCKS; j++){
             int temp;
             cin >> temp;
             direction.push_back(temp);
         }
+        int result = click(direction, 0);
+        if(result >= INF) cout << -1 << endl;
+        else cout << result << endl;
     }
 }
