2ccef23e556a1fad423bf225bd618b6af8ec095e
kimtaesan <ekdlqld21@naver.com>
2021-09-25 16:13:30 +0900

    9/25 제 7회 한기대 프로그래밍 대회 F번

--- 프로그래밍 대회/학교 프로그래밍 경시대회/netcome.cpp
+++ 프로그래밍 대회/학교 프로그래밍 경시대회/netcome.cpp
@@ -1,17 +1,30 @@
+/****************************************
+    
+    제 7회 한기대 프로그래밍 대회 F번 문제
+
+    Input : total wage, score of company
+
+    Output : netcome
+
+    Calculates netcome
+
+
+    
+    9/25 coded by bmkim
+    
+******************************************/
 #include <iostream>
 
 using namespace std;
 
 int solution(int p, int k){
-    int n = p;
-    while(true){
-        int ret = n / 3 + 3 * (n / k);
-        if(ret == p)
-            return n;
-        if(ret > p * k)
-            return -1;
+    int n = (int) (3.0*p/(1+(9.0/k)));
+    while(n/3 + n/k*3 < p){
         n++;
     }
+    if(n/3 + n/k*3 == p)
+        return n;
+    return -1;
 }
 
 int main(){
