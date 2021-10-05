cdafc37dfd07c731dfff949b4570ffb74283e8e4
kimtaesan <ekdlqld21@naver.com>
2021-10-05 21:49:48 +0900

    10/5 Programmers Lv.3 이중 우선순위 큐 using multiset

---
+++ 프로그래머스/twinPQ_multiset.cpp
@@ -0,0 +1,41 @@
+/****************************************
+    
+    Programmers Lv3. 이중우선순위 큐
+
+    Input : vector<string> operations
+
+    Output : [max elem, min elem]
+
+    Develop the Twin Priority Queue using multiset
+
+    
+    10/5 coded by bmkim
+    
+******************************************/
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+vector<int> solution(vector<string> arguments) {
+    vector<int> answer;
+    multiset<int> que;
+    multiset<int>::iterator iter;
+    string sub;
+
+    for(auto s : arguments) {
+        sub =s.substr(0, 2);
+        if(sub=="I ") que.insert(stoi(s.substr(2,s.length()-2))); 
+        else if(s.substr(2,1)=="1"&&que.size()>0) { que.erase(--que.end()); }
+        else if(que.size()>0) { que.erase(que.begin()); }
+    }
+
+    if(que.size()==0) { answer.push_back(0); answer.push_back(0); }
+    else { 
+        iter = --que.end(); answer.push_back(*iter); 
+        iter = que.begin(); answer.push_back(*iter);
+    }
+
+    return answer;
+}
