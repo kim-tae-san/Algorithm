97777b0740a5fbf0321e3af0a27777d8535afd0f
kimtaesan <ekdlqld21@naver.com>
2022-01-12 20:46:50 +0900

    1/12 edit

--- 프로그래머스/trippath.cpp
+++ 프로그래머스/trippath.cpp
@@ -8,21 +8,22 @@ using namespace std;
 
 vector<vector<string> > v;
 bool visited[10001] = {0,};
-vector<vector<string> > ans;
+vector<string> ans;
 
 void dfs(int cnt, int n, vector<vector<string> >& tickets){
+    // 모든 티켓을 사용한 경우 종료 조건
     if(cnt == n-1){
-        vector<string> temp;
+        vector<string> ans;
         for(auto i:v){
-            temp.push_back(i[0]);
+            ans.push_back(i[0]);
         }
-        temp.push_back(v[v.size()-1][1]);
-        ans.push_back(temp);
+        ans.push_back(v[v.size()-1][1]);
         return;
     }
     for(int i=0; i<n; i++){
+        // 방문했거나 이전 티켓의 목적지와 현재 티켓의 출발지가 일치하지 않을 경우 다음 티켓으로
         if(visited[i] || v[v.size()-1][1] != tickets[i][0]) continue;
-        //cout << v[v.size()-1][1] << " " << tickets[i][0] << endl;
+        cout << v[v.size()-1][0] << " " << v[v.size()-1][1] << " " << cnt << " "  << i << endl;
         visited[i] = true;
         v.push_back(tickets[i]);
         dfs(cnt + 1, n, tickets);
@@ -34,10 +35,11 @@ void dfs(int cnt, int n, vector<vector<string> >& tickets){
 vector<string> solution(vector<vector<string> > tickets) {
     // 알파벳 순으로 정렬후 깊이 우선 탐색으로 처음 발견하는 경로가 정답
     sort(tickets.begin(), tickets.end());
-    /*for(auto i: tickets){
-        cout << i[0] << " " << i[1] << endl << endl;
-    }*/
+    vector<int> start;
     int n;
+    for(auto i: tickets){
+        cout << i[0] << " " << i[1] << endl << endl;
+    }
     for(int i=0; i<tickets.size(); i++){
         if(tickets[i][0] == "ICN"){
             n = i;
@@ -47,11 +49,14 @@ vector<string> solution(vector<vector<string> > tickets) {
     v.push_back(tickets[n]);
     visited[n] = true;
     dfs(0, tickets.size(), tickets);
-    
-    return ans[0];
+
+    for(auto i:ans)
+        cout << i << endl;
+    return ans;
 }
 
 int main(){
-    vector<vector<string> > tickets = {{"ICN", "SFO"}, {"ICN", "SFO"}, };
+    vector<vector<string> > tickets = {{"ICN", "BOO"}, {"ICN", "COO"}, {"COO", "DOO"}, {"DOO", "COO"}, {"BOO", "DOO"}, {"DOO", "BOO"}, {"BOO", "ICN"}, {"COO", "BOO"}};
+
     solution(tickets);
 }
